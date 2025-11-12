d64560c8cdbae8b289cc0ebec9a079e42456a4d9
C6OI <C6OI@yandex.ru>
2025-11-12 21:09:59 +0500

    Always log exceptions with stack trace

--- Core/Program.cs
+++ Core/Program.cs
@@ -10,11 +10,6 @@ try {
     });
 } catch (Exception e) {
     AnsiConsole.WriteException(e, new ExceptionSettings {
-#if DEBUG
-        Format = ExceptionFormats.Default,
-#else
-        Format = ExceptionFormats.NoStackTrace,
-#endif
         Style = new ExceptionStyle {
             Message = Style.Parse("red")
         }
